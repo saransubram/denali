4c8e7bc60d29f53aed465644a6cf6be40d4ed268
saransubram <saransubram@gmail.com>
2017-01-13 07:49:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
 from flask import request
 from flask import make_response
 
@@ -35,39 +42,8 @@ def makeWebhookResult(req):
     print(speech)
 
     return {
-        
         "speech": speech,
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-denali"
-    }
-
-def makeWebhookResult2(req):
-    if req.get("result").get("action") != "employee.stats":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    var1 = parameters.get("stats")
-    
-    var2 = {'Min':27, 'Max':59, 'Mean':38.4}
-
-    speech = "The answer for " + var1 + " is " + str(var2[var1])
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-denali"
-    }
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
-    app.run(debug=True, port=port, host='0.0.0.0')
+        "source": "apiai-miibot"
