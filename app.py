19afee138574f76cbd0059c78145b0e4a2226975
saransubram <saransubram@gmail.com>
2017-01-13 11:39:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def makeWebhookResult(req):
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-miibot"
+        "source": "apiai-denali"
     }
 
 
